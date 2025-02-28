e3e3c8476f234eb962510f4839f01b212c9a3d1e
Rodrigo Tagliavini <falecomigo@gmail.com>
2025-02-28 16:29:41 -0300

    pong

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -214,3 +214,133 @@ document.addEventListener("DOMContentLoaded", function () {
     createStars("stars-final");
     createComets("comets-final");
 });
+
+const canvas = document.getElementById("pongCanvas");
+const ctx = canvas.getContext("2d");
+
+let score1 = 0, score2 = 0;
+let gameWidth, gameHeight, paddleWidth, paddleHeight, ballRadius;
+
+function resizeGameArea() {
+    const container = document.querySelector(".content-container");
+    canvas.width = container.clientWidth;
+    canvas.height = container.clientHeight;
+    updateSizes();
+}
+
+function updateSizes() {
+    gameWidth = canvas.width;
+    gameHeight = canvas.height;
+
+    paddleWidth = gameWidth * 0.008;
+    paddleHeight = gameHeight * 0.25;
+    ballRadius = gameWidth * 0.007;
+
+    paddleLeft.width = paddleWidth;
+    paddleLeft.height = paddleHeight;
+    paddleLeft.x = gameWidth * 0.02;
+    paddleLeft.y = (gameHeight - paddleHeight) / 2;
+
+    paddleRight.width = paddleWidth;
+    paddleRight.height = paddleHeight;
+    paddleRight.x = gameWidth - gameWidth * 0.02 - paddleWidth;
+    paddleRight.y = (gameHeight - paddleHeight) / 2;
+
+    ball.radius = ballRadius;
+    resetBall();
+}
+
+function resetBall(winner) {
+    if (winner === "left") {
+        score1++;
+    } else if (winner === "right") {
+        score2++;
+    }
+    document.getElementById("score1").innerText = score1;
+    document.getElementById("score2").innerText = score2;
+
+    ball.x = gameWidth / 2;
+    ball.y = gameHeight / 2;
+    ball.speedX = (Math.random() > 0.5 ? 1 : -1) * (gameWidth * 0.005);
+    ball.speedY = (Math.random() > 0.5 ? 1 : -1) * (gameHeight * 0.005);
+}
+
+let ball = { x: 0, y: 0, speedX: 4, speedY: 4, radius: 10 };
+
+let paddleLeft = { x: 0, y: 0, width: 10, height: 60, speed: 0.15, errorMargin: 10 };
+let paddleRight = { x: 0, y: 0, width: 10, height: 60, speed: 0.08, delay: 10, targetY: 0, errorMargin: 25 };
+
+function drawDashedLine() {
+    ctx.strokeStyle = "white";
+    ctx.setLineDash([10, 10]);
+    ctx.beginPath();
+    ctx.moveTo(gameWidth / 2, 0);
+    ctx.lineTo(gameWidth / 2, gameHeight);
+    ctx.stroke();
+    ctx.setLineDash([]);
+}
+
+function drawElements() {
+    ctx.clearRect(0, 0, gameWidth, gameHeight);
+    drawDashedLine();
+    ctx.fillStyle = "white";
+    ctx.fillRect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
+    ctx.fillRect(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height);
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function update() {
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+
+    if (ball.x < 0) {
+        resetBall("right");
+        return;
+    }
+    if (ball.x > gameWidth) {
+        resetBall("left");
+        return;
+    }
+    if (ball.y - ball.radius < 0 || ball.y + ball.radius > gameHeight) {
+        ball.speedY *= -1;
+    }
+
+    function checkPaddleCollision(paddle) {
+        let missChance = Math.random() * paddle.errorMargin;
+        return (
+            ball.y > paddle.y - missChance &&
+            ball.y < paddle.y + paddle.height + missChance
+        );
+    }
+
+    if (ball.x - ball.radius < paddleLeft.x + paddleLeft.width) {
+        if (checkPaddleCollision(paddleLeft)) {
+            ball.speedX *= -1;
+        } else {
+            resetBall("right");
+        }
+    }
+
+    if (ball.x + ball.radius > paddleRight.x) {
+        if (checkPaddleCollision(paddleRight)) {
+            ball.speedX *= -1;
+        } else {
+            resetBall("left");
+        }
+    }
+
+    paddleLeft.y += (ball.y - paddleLeft.y - paddleLeft.height / 2) * paddleLeft.speed;
+    paddleRight.y += (ball.y - paddleRight.y - paddleRight.height / 2) * paddleRight.speed;
+}
+
+function gameLoop() {
+    update();
+    drawElements();
+    requestAnimationFrame(gameLoop);
+}
+
+window.addEventListener("resize", resizeGameArea);
+resizeGameArea();
+gameLoop();
